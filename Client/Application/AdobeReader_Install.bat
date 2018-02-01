5c046d1fd0d29145e638d3c479a7ad67ca37824b
jenovava <tt@jenova.biz>
2018-02-01 15:51:48 +0900

    Update AdobeReader_Install.bat

--- Client/Application/AdobeReader_Install.bat
+++ Client/Application/AdobeReader_Install.bat
@@ -1,7 +1,7 @@
 echo Adobe Reader10.
 taskkill /fi "imagename eq iexplore.exe"
 taskkill /fi "imagename eq acrord32.exe"
-pushd \\pu24sf10\sf10dn\DCS_WORK\suzu\Adobe_X
+pushd \\pu******\******\DCS_WORK\suzu\Adobe_X
 AdbeRdr1001_ja_JP.exe /sAll /rs
 msiexec /i FontPack1000_ko_KR.msi /passive /norestart
 msiexec /i install_flash_player_10_plugin.msi /passive /norestart
